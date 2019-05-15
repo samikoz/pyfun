e824f25223363a47203fecb6d680bbcf3157f765
sami <skozlowski@togetherdata.com>
2019-05-15 17:35:44 +0200

    Implemented transitioning red background
    In case of a validation error

---
+++ dispenser/frontend/static_files/amount-handling.js
@@ -0,0 +1,54 @@
+const selfEndpoint = 'http://localhost:8000';
+
+const send_button = document.getElementById('send_button');
+send_button.addEventListener('click', dispenseRequestedAmount);
+
+function dispenseRequestedAmount() {
+    validate(getRequestedAmount())
+    .then(navigateToDispensedPage())
+    .catch(reddenForm);
+}
+
+function validate(amount) {
+    const validationEndpoint = `${selfEndpoint}/validate`;
+    return fetch(`${validationEndpoint}?amount=${amount}`)
+    .then(retrieveResponseContent);
+}
+
+function getRequestedAmount() {
+    return document.getElementById('requested_amount').value;
+}
+
+function retrieveResponseContent(response) {
+    if (response.ok) {
+        return response.text();
+    } else {
+        return response.text().then(err => Promise.reject(err));
+    }
+}
+
+function navigateToDispensedPage(amount) {
+    window.location.href = `${selfEndpoint}/dispensed?amount=${amount}`;
+}
+
+function reddenForm(errMsg) {
+    const inputFieldID = 'requested_amount';
+    switchOffTransitionProperty(inputFieldID);
+    turnFormBackgroundRedAndSwitchOnTransition(inputFieldID);
+    turnFormBackgroundWhite(inputFieldID);
+    console.log(`Error: ${errMsg}`);
+}
+
+function switchOffTransitionProperty(elementId) {
+    document.getElementById(elementId).classList.remove('transitionable');
+}
+
+function turnFormBackgroundRedAndSwitchOnTransition(elementId) {
+    const element = document.getElementById(elementId);
+    element.style['background-color'] = 'red';
+    element.classList.add('transitionable');
+}
+
+function turnFormBackgroundWhite(elementId) {
+    document.getElementById(elementId).style['background-color'] = 'white';
+}
