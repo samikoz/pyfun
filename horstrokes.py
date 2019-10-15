9545d990a9874eb4d39063f12ba3c3b8eae1fce3
samikoz <kozlosam@gmail.com>
2019-10-15 12:45:38 +0200

    Horstrokes, split function
    Reimplemented using split_at from more_itertools

--- horstrokes.py
+++ horstrokes.py
@@ -1,25 +1,16 @@
 # q2 from future3 coding contest, 19.10.17
 
 import itertools
+import more_itertools
 
 
 def split(array, separator=None):
     """splits an array into subarrays separated by a separator.
-    empty subarrays are not returned."""
-
-    count = array.count(separator)
-    char_indices = []
-    offset = -1
-    for _ in itertools.repeat(None, count):
-        char_indices.append(array.index(separator, offset+1))
-        offset = char_indices[-1]
-
-    char_indices.insert(0, -1)
-    char_indices.insert(count+1, len(array))
-
-    splits = [array[char_indices[i]+1:char_indices[i+1]] for i in range(count+1)
-              if char_indices[i+1] > char_indices[i]+1]
-    return splits
+        empty subarrays are not returned."""
+    return list(itertools.filterfalse(
+        lambda arr: arr == [],
+        more_itertools.split_at(array, lambda c: c == separator)
+    ))
 
 
 def no_of_horizontal_strokes(heights):
--- test/test_horstrokes.py
+++ test/test_horstrokes.py
@@ -6,14 +6,14 @@ from horstrokes import no_of_horizontal_strokes, split
 class TestHorStrokes:
 
     @pytest.mark.parametrize('splittee,sep,splitted', [
-        ((1, 2, 3, 4, 5, 6), 3, [(1, 2), (4, 5, 6)]),
+        ((1, 2, 3, 4, 5, 6), 3, [[1, 2], [4, 5, 6]]),
         ([1, 1, 2, 1, 7, 8, 1, 7], 1, [[2, ], [7, 8], [7, ]])
     ])
     def test_split_correctness(self, splittee, sep, splitted):
         assert split(splittee, sep) == splitted
 
     @pytest.mark.parametrize('splittee,sep,splitted', [
-        ((1, 2, 3, 4), 8, [(1, 2, 3, 4)]),
+        ((1, 2, 3, 4), 8, [[1, 2, 3, 4]]),
     ])
     def test_split_separator(self, splittee, sep, splitted):
         assert split(splittee, sep) == splitted
